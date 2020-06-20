abb78ad73344830efab4216f7d9f9ffde5646d4a
Náthali Souza <naah.ms@outlook.com>
2020-06-20 00:23:36 -0300

    Versão 2.0
    Não terminada...

---
+++ Assets/Origem/Scripts/Espada.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Espada : MonoBehaviour
+{
+    public float dano;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Inimigo"))
+        {
+            Vida vidaInimigo = other.gameObject.GetComponent<Vida>();
+            vidaInimigo.vida = vidaInimigo.vida - dano;
+            
+        }
+    }
+}
---
+++ Assets/Origem/Scripts/IA.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IA : MonoBehaviour
+{
+   public enum EstadoIA
+    {
+        Atacando,
+        Andando,
+    }
+
+    public EstadoIA estado;
+    public float dano;
+    public Animator anim;
+    bool bateu;
+
+    NavMeshAgent agenteNM;
+    Vida vida;
+    Vida vidaJogador;
+
+
+    void Awake()
+    {
+        agenteNM = GetComponent<NavMeshAgent>();
+        vida = GetComponent<Vida>();
+        vidaJogador = GameObject.FindWithTag("Player").GetComponent<Vida>();
+    }
+
+    void Update()
+    {
+        if (agenteNM.isStopped)
+        {
+            estado = EstadoIA.Atacando;
+        } else
+        {
+            estado = EstadoIA.Andando;
+        }
+
+        if(estado == EstadoIA.Atacando)
+        {
+            anim.SetTrigger("atacou");
+            bateu = true;
+            
+            if (bateu == true)
+            {
+                vidaJogador.vida = vidaJogador.vida - dano * Time.deltaTime;
+            }
+           
+        }
+
+        if (vida.vida <= 0)
+        {
+            anim.SetTrigger("morreu");
+            Destroy(gameObject, 3f);
+
+        }
+    }
+}
+
+internal class Bateu
+{
+}
